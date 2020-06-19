3cbd24f96e49bb500794d07cf2a883ca7efb9d33
stem league <64894084+stemleague@users.noreply.github.com>
2020-06-18 21:34:22 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// The roar() function happens ONLY when the yeti element is clicked. This function tells the browser (Chrome, Safari, Firefox, etc.) to open an alert box that says "RAAAAAAWWWWRRRRR"
+
+function roar(){
+  alert("RAAAAAAWWWWRRRRR");
+}
+
+// Check out https://www.w3schools.com/js/js_popup.asp to learn more about alerts and other popup boxes
